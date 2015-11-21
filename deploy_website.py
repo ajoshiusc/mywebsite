b0125b191613c23bbb793f07a8daa416e2c08a2a
Anand A Joshi <ajoshi.usc@outlook.com>
2015-11-21 02:51:43 -0800

    updated code to work with python 3, but not sure if it works.

--- deploy_website.py
+++ deploy_website.py
@@ -6,36 +6,36 @@ import distutils.core
 import shutil
 
 if len(sys.argv) != 3:
-    print "Exactly two inputs are required. See usage below:\n"
-    print "\tpython deply_website.py <jemdoc_source_dir> <html_output_dir>\n"
+    print("Exactly two inputs are required. See usage below:\n")
+    print("\tpython deply_website.py <jemdoc_source_dir> <html_output_dir>\n")
     exit(1)
 
 src_dir = sys.argv[1]
 out_dir = sys.argv[2]
 
 if not os.path.isdir(src_dir):
-    print "\n<jemdoc_source_dir> does not exists: " + src_dir
+    print ("\n<jemdoc_source_dir> does not exists: " + src_dir)
     exit(1)
 
 if not os.path.isdir(out_dir):
-    print "\n<html_output_dir> does not exists: " + out_dir
+    print ("\n<html_output_dir> does not exists: " + out_dir)
     exit(1)
 
 
 # delete all html/css files in out_dir
 for fl in glob.glob(out_dir + "/*.html"):
-    print "Deleting " + fl
+    print ("Deleting " + fl)
     os.remove(fl)
 
 for fl in glob.glob(out_dir + "/*.css"):
-    print "Deleting " + fl
+    print ("Deleting " + fl)
     os.remove(fl)
 
 pw_dir = os.getcwd()
 os.chdir(src_dir)
 for fl in glob.glob("*.jemdoc"):
     syscmd = "python C:\\Users\\ajoshi\\PycharmProjects\\mywebsite\\jemdoc.py -c mysite.conf -o C:\\Users\\ajoshi\\PycharmProjects\\mywebsite\\" + out_dir + "\ " + fl
-    print syscmd
+    print (syscmd)
     os.system(syscmd)
 
 os.chdir(pw_dir)
